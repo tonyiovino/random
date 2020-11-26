41b436c334b12b91e794afa758008e1e2364a5ee
tonyiovino <antonioiovino2004tony@gmail.com>
2020-11-26 20:31:51 +0100

    Random

---
+++ rand.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int num;
+    int min, max;
+    int i;
+    
+    srand( time(NULL) );
+
+    min = 50, max = 56;
+    printf("Gruppo a:");
+    for (i = 0; i < 5; i++){
+        num = min + rand() % (max - min);
+        printf("%4d", num);
+    }
+
+    min = 1, max = 6;
+    printf("\nGruppo b:");
+    for (i = 0; i < 5; i++){
+        num = (min + rand() % (max - min)) * 2;
+        printf("%4d", num);
+    }
+
+    min = 1, max = 6;
+    printf("\nGruppo c:");
+    for (i = 0; i < 5; i++){
+        num = min + ((1 + rand() % (max - min)) * 2);
+        printf("%4d", num);
+    }
+
+    min = 3, max = 12;
+    printf("\nGruppo d:");
+    for (i = 0; i < 5; i++){
+        num = (min + rand() % (max - min)) * 2;
+        printf("%4d", num);
+    }
+
+    putchar('\n');
+
+
+    return 0;
+}
